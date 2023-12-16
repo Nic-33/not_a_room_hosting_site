d7c38d66ea39a40eecd4ee7ba372d3156a3a7c38
Nicholas Gould <nicgou@gmail.com>
2023-12-16 15:48:07 -0500

    completed backend phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,16 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
 // router.get('/hello/world', function (req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Hello World!');
 // });
 
 // Add a XSRF-TOKEN cookie
+
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
@@ -15,5 +18,7 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+
 
 module.exports = router;
